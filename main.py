c079b13896705678f598f5db2af938e8ec6e2c33
reflexmills <lodog5378@gmail.com>
2025-06-06 21:40:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -711,7 +711,7 @@ def run_web_server():
         conn.close()
         return "OK", 200
 
-    port = int(os.environ.get("PORT", 10002))
+    port = int(os.environ.get("PORT", 2500))
     app.run(host="0.0.0.0", port=port)
 
 def main():
